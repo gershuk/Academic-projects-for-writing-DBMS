63c77a7b3e3e128de696347418bd7277f15e7cca
Anton Melnikov <antonhehog@gmail.com>
2019-10-09 23:25:16 +1000

    add testing framework show results command

---
+++ TestingFramework/Models.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+namespace TestingFramework
+{
+    public class Test
+    {
+        public string Input { get; set; }
+        public string Output { get; set; }
+        public string Status { get; set; }
+        public bool ExpectOutput { get; set; }
+    }
+
+
+    class TestResult
+    {
+        public Test UsedTest { get; set; }
+        public string ReturnedOutput { get; set; }
+        public string ReturnedStatus { get; set; }
+        public bool TestPassed { get; set; }
+
+        public TestResult() { }
+
+        public TestResult(Test t, string output, string status, bool passed)
+        {
+            UsedTest = t;
+            ReturnedOutput = output;
+            ReturnedStatus = status;
+            TestPassed = passed;
+        }
+
+        public string ToJson() => JsonConvert.SerializeObject(this);
+    }
+}

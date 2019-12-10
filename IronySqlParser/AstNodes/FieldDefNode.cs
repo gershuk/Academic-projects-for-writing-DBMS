4b6e8ce0b11de8a49a6adfdd38c54b4350bed706
Vladislav <vladik.morozov.2012@mail.ru>
2019-12-10 21:11:06 +1000

    Hot fix Id field in FieldDefNode

--- IronySqlParser/AstNodes/FieldDefNode.cs
+++ IronySqlParser/AstNodes/FieldDefNode.cs
@@ -5,7 +5,7 @@ namespace IronySqlParser.AstNodes
 {
     public class FieldDefNode : SqlNode
     {
-        public List<string> Id { get; set; }
+        public Id Id { get; set; }
         public DataType FieldType { get; set; }
         public double? TypeParamOpt { get; set; }
         public List<string> ConstaraintList { get; set; }
@@ -13,7 +13,7 @@ namespace IronySqlParser.AstNodes
 
         public override void CollectInfoFromChild ()
         {
-            Id = FindFirstChildNodeByType<IdNode>().Id;
+            Id = new Id (FindFirstChildNodeByType<IdNode>().Id);
             FieldType = FindFirstChildNodeByType<TypeNameNode>().FieldType;
             TypeParamOpt = FindFirstChildNodeByType<TypeParamsOptNode>().TypeParamOpt;
             ConstaraintList = FindFirstChildNodeByType<ConstraintListOptNodes>().ConstraintList;

1c7fde96ab0e0bc4327df4f1064122a9db1e8598
kenny5660 <32442117+kenny5660@users.noreply.github.com>
2019-12-26 00:30:45 +1000

    fix multithread test

--- IntegrationTests/MultyThreadTest.cs
+++ IntegrationTests/MultyThreadTest.cs
@@ -30,12 +30,12 @@ namespace IntegrationTests
             DelFiles();
             var _core = new DataBase(20, new DataBaseEngineMain(_testPath), new TransactionScheduler());
             var expected = GetTestData();
-            var n = 10;
+            var n = 2;
             var cl = Enumerable.Range(0, n).Select(i=>new TestClient($"cl{i}", _core)).ToArray();
             cl[1].SendQuery("create table test (i int,c char(40),d double)");
 
             var query = "";
-            for (var i = 0; i < 100; i++)
+            for (var i = 0; i < 10; i++)
             {
                 query += $"insert into test values ({i},'{i}',{i * 1.0 + 0.1.ToString().Replace(',', '.')})\n";
             }
@@ -44,14 +44,15 @@ namespace IntegrationTests
             Console.WriteLine("insert end");
            var tasks = Enumerable.Range(0, n).Select(
                 i => Task.Run(() => SendSQLQuery(cl[i], $"BEGIN TRANSACTION cl2\n" +
-                $"update test set test.i = -1 where c='{i}';" +
+                $"update test set i = -1 where i = {i};" +
                 "select * from test;" +
                 $"commit",
                 expected))).ToArray();
             Task.WaitAll(tasks);
 
 
-        }
+        
+                }
 
     }
 }
